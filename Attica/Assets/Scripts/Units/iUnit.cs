90bffb180f4a8ed70e401fd70d3f63bdc34123f5
wguiles <36618803+wguiles@users.noreply.github.com>
2021-02-06 00:48:38 -0600

    basic framework for unit scripts

---
+++ Attica/Assets/Scripts/Units/iUnit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface iUnit 
+{
+    int GetHealth();
+
+    void SetHealth(int newHealth);
+
+    bool canAttack();
+
+    void Action();
+
+    int GetCost();
+
+    void SetCost(int newCost);
+
+    float GetBuildSpeed();
+
+    void SetBuildSpeed(float newBuildSpeed);
+
+    void build();
+}
