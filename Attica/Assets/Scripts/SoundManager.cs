6810b2655b29d63676e20b9a8bb26957c69f25e9
wguiles <36618803+wguiles@users.noreply.github.com>
2021-02-05 22:37:58 -0600

    Soundmanager commited

---
+++ Attica/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public SoundManager instance;
+
+    public Sound[] sounds;
+
+
+    private void Awake()
+    {
+        instance = this;
+
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void play (string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+
+        if (s == null)
+            return;
+
+        s.source.Play();
+    }
+}
