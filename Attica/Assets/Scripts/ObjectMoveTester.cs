f6ad3d45009adbd21d501b357bfcb3148e7e07f8
JakeVelicer <007velicer@gmail.com>
2021-02-06 01:21:10 -0600

    Base Movement

---
+++ Attica/Assets/Scripts/ObjectMoveTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMoveTester : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = PathMovement.GetMovement(gameObject.transform, Direction.Up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = PathMovement.GetMovement(gameObject.transform, Direction.Down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = PathMovement.GetMovement(gameObject.transform, Direction.Left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = PathMovement.GetMovement(gameObject.transform, Direction.Right);
+        }
+    }
+}
---
+++ Attica/Assets/Scripts/PathMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction: int
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public static class PathMovement
+{
+    public static Vector3 GetMovement(Transform entity, Direction givenDirection)
+    {
+        Vector3 destination = new Vector3(0, 0.25f, 0);
+
+        switch(givenDirection)
+        {
+            case Direction.Up:
+                destination = entity.position += new Vector3(0.5f, 0.25f, 0);
+                break;
+
+            case Direction.Down:
+                destination = entity.position += new Vector3(-0.5f, -0.25f, 0);
+                break;
+
+            case Direction.Right:
+                destination = entity.position += new Vector3(0.5f, -0.25f, 0);
+                break;
+
+            case Direction.Left:
+                destination = entity.position += new Vector3(-0.5f, 0.25f, 0);
+                break;
+        }
+        return destination;
+    }
+}
