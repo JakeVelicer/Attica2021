f842a733ef358f0f9808e295730e2d2c72a8c7e6
wguiles <36618803+wguiles@users.noreply.github.com>
2021-02-05 22:10:44 -0600

    Scripts folder and beginning of object pooler

---
+++ Attica/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> pooldictionary;
+
+    void Start()
+    {
+        pooldictionary = new Dictionary<string, Queue<GameObject>>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
