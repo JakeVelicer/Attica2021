70f01f3997ebe954c3329e2cc0f418d524092188
JakeVelicer <007velicer@gmail.com>
2021-02-06 17:28:37 -0600

    Full Movement System

--- Attica/Assets/Scripts/PathMovement.cs
+++ Attica/Assets/Scripts/PathMovement.cs
@@ -12,28 +12,57 @@ public enum Direction: int
 
 public static class PathMovement
 {
+    public static Vector3 GetMovementOverDistance(Transform entity, Direction givenDirection, int travel)
+    {
+        Transform destination = entity;
+        for (int i = 0; i < travel; i++)
+        {
+            destination.position = GetMovement(destination, givenDirection);
+        }
+        return destination.position;
+    }
+
     public static Vector3 GetMovement(Transform entity, Direction givenDirection)
     {
         Vector3 destination = new Vector3(0, 0.25f, 0);
-
+        Vector3 entityPos = entity.position;
         switch(givenDirection)
         {
             case Direction.Up:
-                destination = entity.position += new Vector3(0.5f, 0.25f, 0);
+                destination = entityPos += new Vector3(0.5f, 0.25f, 0);
                 break;
 
             case Direction.Down:
-                destination = entity.position += new Vector3(-0.5f, -0.25f, 0);
+                destination = entityPos += new Vector3(-0.5f, -0.25f, 0);
                 break;
 
             case Direction.Right:
-                destination = entity.position += new Vector3(0.5f, -0.25f, 0);
+                destination = entityPos += new Vector3(0.5f, -0.25f, 0);
                 break;
 
             case Direction.Left:
-                destination = entity.position += new Vector3(-0.5f, 0.25f, 0);
+                destination = entityPos += new Vector3(-0.5f, 0.25f, 0);
                 break;
         }
         return destination;
+    }
+
+    public static IEnumerator Move(Transform entity, Direction givenDirection, int givenTravel, float givenSpeed)
+    {
+        Vector3 startPosition = entity.position;
+        Vector3 endPosition = PathMovement.GetMovementOverDistance(entity, givenDirection, givenTravel);
+        float waitTime = givenSpeed / 0.5f;
+        
+        float elapsedTime = 0;
+        
+        while (elapsedTime < waitTime)
+        {
+            entity.position = Vector3.Lerp(startPosition, endPosition, (elapsedTime / waitTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }  
+
+        // Make sure we got there
+        entity.position = endPosition;
     }
 }
---
+++ Attica/Assets/Scripts/Test/ObjectMoveDistanceTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMoveDistanceTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PathMovement.Move(transform, Direction.Up, 5, 1.5f));
+    }
+
+}
