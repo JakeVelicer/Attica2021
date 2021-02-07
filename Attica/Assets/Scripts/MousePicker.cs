614d8e6a8c8115d175f2ffabb0c6a793c81d39a1
JakeVelicer <007velicer@gmail.com>
2021-02-06 22:09:18 -0600

    Mouse Selection

---
+++ Attica/Assets/Scripts/MousePicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePicker : MonoBehaviour
+{
+    public Camera cam;
+    private Ray2D ray;
+    private RaycastHit2D hit;
+    private GameObject selectedTile;
+
+    private void Update()
+    {
+        Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        hit = Physics2D.Raycast(mousePosition, cam.transform.position - mousePosition , 0,001);
+        if (hit == true && selectedTile == null)
+        {
+            selectedTile = hit.transform.gameObject;
+            selectedTile.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else if(hit == true && hit.transform.gameObject != selectedTile && selectedTile != null)
+        {
+            selectedTile.GetComponent<SpriteRenderer>().enabled = false;
+            selectedTile = null;
+            selectedTile = hit.transform.gameObject;
+            selectedTile.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else if (hit == false && selectedTile != null)
+        {
+            selectedTile.GetComponent<SpriteRenderer>().enabled = false;
+            selectedTile = null;
+        }
+        else if (hit == false)
+        {
+            selectedTile = null;
+        }
+    }
+}
